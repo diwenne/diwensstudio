0df237f29359d2b8fd155be4b01855c5d3fc2f7a
diwenne <diwenh5@icloud.com>
2025-09-04 20:40:53 -0700

    scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from './components/ScrollToTop'; 
 
 export default function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <main>
         <Routes>
